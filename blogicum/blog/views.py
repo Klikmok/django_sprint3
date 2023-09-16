4d4d4a7cc4290add70a4c093ed51787b35552ba4
Klikmok <ribakov2007@icloud.com>
2023-09-16 23:52:37 +0500

    Первый пуш проекта на ревью

---
+++ blogicum/blog/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render, get_object_or_404
+
+from blog.models import Post, Category
+
+from datetime import datetime
+
+
+def index(request):
+    post_list = Post.objects.filter(
+        pub_date__lt=datetime.now(),
+        is_published=True,
+        category__is_published=True
+    ).order_by('created_at')[0:5]
+    context = {
+        'post_list': post_list
+    }
+    template = 'blog/index.html'
+    return render(request, template, context)
+
+
+def category_posts(request, category_slug):
+    template = 'blog/category.html'
+    category = get_object_or_404(
+        Category.objects,
+        slug=category_slug,
+        is_published=True
+    )
+    post_list = Post.objects.select_related('category').filter(
+        pub_date__lt=datetime.now(),
+        category__is_published=True,
+        category__slug=category_slug,
+        is_published=True
+        )
+
+    context = {
+        'category': category,
+        'post_list': post_list
+    }
+    return render(request, template, context)
+
+
+def post_detail(request, pk):
+    template = 'blog/detail.html'
+    post = get_object_or_404(
+        Post.objects.select_related('category').order_by('category'),
+        pk=pk,
+        is_published=True,
+        category__is_published=True,
+        pub_date__lt=datetime.now()
+    )
+    context = {
+        'post': post
+    }
+    return render(request, template, context)
