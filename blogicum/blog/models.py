1e1a51468e21cc5b04c6ca9a054c5cde7374189f
Klikmok <ribakov2007@icloud.com>
2023-09-16 23:55:33 +0500

    1.1 Пуш проекта на ревью

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -57,7 +57,8 @@ class Post(models.Model):
     text = models.TextField(verbose_name='Текст')
     pub_date = models.DateTimeField(
         verbose_name='Дата и время публикации',
-        help_text=('Если установить дату и время в будущем — можно делать отложенные публикации.'))
+        help_text=('Если установить дату и время в будущем '
+                   '— можно делать отложенные публикации.'))
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
